ebad0b2796120e1700fee7042f895873c80758d6
Leandro <souzaleandro79@gmail.com>
2022-07-05 12:31:15 -0300

    teste preenchimento de cadastro

--- src/test/java/pages/CadastroPage.java
+++ src/test/java/pages/CadastroPage.java
@@ -1,4 +1,55 @@
 package pages;
 
-public class CadastroPage {
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import support.Utils;
+
+public class CadastroPage extends Utils {
+    WebDriver driver;
+
+    private By senhor = By.id("uniform-id_gender1");
+    private By Sra = By.id("uniform-id_gender2");
+    private By nome1 = By.id("customer_firstname");
+    private By sobreNome2 = By.id("customer_lastname");
+    private By senha3 = By.id("passwd");
+    private By aniversarioDia = By.id("days");
+    private By aniversarioMes = By.id("months");
+    private By aniversarioAno = By.id("years");
+
+    public CadastroPage(WebDriver driver) {
+        this.driver = driver;
+    }
+    public void selecionarTitulo( int titulo){
+        waitPresent(senhor,20);
+        if (titulo == 1){
+            driver.findElement(senhor).click();
+        }else if (titulo == 2) {
+            driver.findElement(Sra).click();
+        }
+    }
+
+    public void preencheNome(String nome){
+        driver.findElement(nome1).sendKeys(nome);
+    }
+    public void preecheSobreNome(String sobreNome){
+        driver.findElement(sobreNome2).sendKeys(sobreNome);
+    }
+    public void preencheSenha(String senha){
+        driver.findElement(senha3).sendKeys(senha);
+    }
+
+    public void selionaDataAniversario(int dia){
+        Select selectDia = new Select(driver.findElement(aniversarioDia));
+        selectDia.selectByIndex(dia);
+    }
+    public void selionaMesAniversario(int mes){
+        Select selectMes = new Select(driver.findElement(aniversarioMes));
+        selectMes.selectByIndex(mes);
+    }
+    public void selionaAnoAniversario(String ano){
+        Select selectAno = new Select(driver.findElement(aniversarioAno));
+        selectAno.selectByValue(ano);
+    }
 }
--- src/test/java/steps/CadastroSteps.java
+++ src/test/java/steps/CadastroSteps.java
@@ -1,14 +1,24 @@
 package steps;
 
-import io.cucumber.java.pt.Dado;
-import io.cucumber.java.pt.E;
+
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
+import pages.CadastroPage;
 
 public class CadastroSteps extends TesteCadastroRun {
 
+    CadastroPage cadastroPage = new CadastroPage(driver);
+
+
     @Quando("preencho o formulário de cadastro")
     public void preenchoOFormulárioDeCadastro() {
+        cadastroPage.selecionarTitulo(1);
+        cadastroPage.preencheNome("Matheus");
+        cadastroPage.preecheSobreNome("Lima");
+        cadastroPage.preencheSenha("hajhvbav2545@");
+        cadastroPage.selionaDataAniversario(25);
+        cadastroPage.selionaMesAniversario(5);
+        cadastroPage.selionaAnoAniversario("1980");
     }
 
     @Entao("vejo a mensagem de cadastro realizado com sucesso")
